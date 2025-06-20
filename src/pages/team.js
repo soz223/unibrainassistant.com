937d3c42710524075760728ebcfb1f6b0d9ef48b
soz223 <soz223@lehigh.edu>
2025-06-20 13:18:34 -0400

    update team

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,133 +1,80 @@
 import React from 'react'
 import { Container, Divider, Grow, Grid } from '@mui/material'
 import PersonCard from '../components/PersonCard'
-// import CbisLogo from '../images/CBIS_logo_blue.png'
-import {Helmet} from "react-helmet";
+import { Helmet } from 'react-helmet'
 
-const titleName = "BrainGB - Team"
+const titleName = "UniBrain - Team"
 
 const TeamPage = () => {
   return (
-      <Container style={
-          {
-              alignItems: 'center',
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-          }
-      }>
-          <Helmet>
-              <title>{titleName}</title>
-          </Helmet>
-          <h1 style={{padding: '40px'}}>Faculty</h1>
-          {/*  Horizontal align PersonCard */}
+    <Container style={{
+      alignItems: 'center',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      <Helmet>
+        <title>{titleName}</title>
+      </Helmet>
 
-          <Divider style={
-              {
-                  width: '100%',
-                  marginBottom: '50px',
-              }
-          }/>
+      <h1 style={{ padding: '40px' }}>Faculty</h1>
+      <Divider style={{ width: '100%', marginBottom: '50px' }} />
 
-          <Grow
-              in={true}
-              style={{ transformOrigin: '0 0 0' }}
-              {...{ timeout: 1000 }}
-          >
+      <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...{ timeout: 1000 }}>
+        <Grid container spacing={1} justifyContent='center'>
+          <PersonCard
+            name="Lifang He"
+            photo="/people/LifangHe.jpg"
+            position="Lehigh University"
+            link="https://engineering.lehigh.edu/faculty/lifang-he"
+          />
+          <PersonCard
+            name="Carl Yang"
+            photo="/people/CarlYang.jpg"
+            position="Emory University"
+            link="https://www.cs.emory.edu/~jyang71/"
+          />
+        </Grid>
+      </Grow>
 
-              <Grid container spacing={1} justifyContent={'center'}>
-                  <PersonCard
-                      name="Lifang He"
-                      photo={'/people/LifangHe.jpg'}
-                      position="Lehigh University"
-                      link={'https://engineering.lehigh.edu/faculty/lifang-he'}
-                  />
+      <h1 style={{ padding: '40px', paddingTop: '100px' }}>Students</h1>
+      <Divider style={{ width: '100%', marginBottom: '50px' }} />
 
-                  <PersonCard
-                      name="Liang Zhan"
-                      photo="/people/LiangZhan.jpeg"
-                      position="University of Pittsburgh"
-                      link={'https://www.engineeringx.pitt.edu/People/Faculty/Profiles/Liang-Zhan/'}
-                  />
-
-                  <PersonCard
-                      name="Ying Guo"
-                      photo="/people/YingGuo.jpg"
-                      position="Emory University"
-                      link={'https://sph.emory.edu/faculty/profile/index.php?FID=ying-guo-629'}
-                  />
-
-                  <PersonCard
-                      name="Carl Yang"
-                      photo="/people/CarlYang.jpg"
-                      title="Founding Director"
-                      position="Emory University"
-                      link="https://www.cs.emory.edu/~jyang71/"
-                  />
-              </Grid>
-          </Grow>
-
-          <h1 style={{padding: '40px', paddingTop: '100px'}}>Students</h1>
-          {/*  Horizontal align PersonCard */}
-
-          <Divider style={
-              {
-                  width: '100%',
-                  marginBottom: '50px',
-              }
-          }/>
-
-          <Grow
-              in={true}
-              style={{ transformOrigin: '0 0 0' }}
-              {...{ timeout: 1000 }}
-          >
-
-              <Grid container spacing={1} justifyContent={'center'} paddingBottom={'20px'}>
-                  <PersonCard
-                      name="Hejie Cui"
-                      photo="/people/HejieCui.jpeg"
-                      position="Emory University"
-                      link={'https://hejiecui.com/'}
-                  />
-
-                  <PersonCard
-                      name="Wei Dai"
-                      photo="/people/WeiDai.png"
-                      position="Emory University"
-                      link={'https://dd.works/'}
-                  />
-
-                  <PersonCard
-                      name="Yanqiao Zhu"
-                      photo="/people/YanqiaoZhu.jpg"
-                      position="UCLA"
-                      link={'https://sxkdz.github.io/'}
-                  />
-
-                  <PersonCard
-                      name="Xuan Kan"
-                      photo="/people/XuanKan.jpeg"
-                      position="Emory University"
-                      link={'http://kanxuan.live/'}
-                  />
-
-                  <PersonCard
-                      name="Tony Chen Gu"
-                      photo="/people/TonyChen.jpeg"
-                      position="Emory University"
-                      link={''}
-                  />
-
-                  <PersonCard
-                      name="Joshua Lukemire"
-                      photo="/people/JoshLukemire.jpg"
-                      position="Emory University"
-                      link={''}
-                  />
-              </Grid>
-          </Grow>
-      </Container>
+      <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...{ timeout: 1000 }}>
+        <Grid container spacing={1} justifyContent='center' paddingBottom='20px'>
+          <PersonCard
+            name="Yao Su"
+            photo="/people/YaoSu.jpg"
+            position="WPI"
+            link="https://www.google.com/search?q=Yao+Su"
+          />
+          <PersonCard
+            name="Keqi Han"
+            photo="/people/KeqiHan.jpg"
+            position="Emory University"
+            link="https://www.google.com/search?q=Keqi+Han"
+          />
+          <PersonCard
+            name="Songlin Zhao"
+            photo="/people/SonglinZhao.jpg"
+            position="Lehigh University"
+            link="https://www.google.com/search?q=Songlin+Zhao"
+          />
+          <PersonCard
+            name="Yurou Dai"
+            photo="/people/YurouDai.jpg"
+            position="Lehigh University"
+            link="https://www.google.com/search?q=Yurou+Dai"
+          />
+          <PersonCard
+            name="Mingjie Zeng"
+            photo="/people/MingjieZeng.jpg"
+            position="WPI"
+            link="https://www.google.com/search?q=Mingjie+Zeng"
+          />
+        </Grid>
+      </Grow>
+    </Container>
   )
 }
 
