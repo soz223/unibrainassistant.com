ffec107d0cce4725a0538ef70cfee56a9d8613fd
soz223 <soz223@lehigh.edu>
2025-06-20 17:01:53 -0400

    update logo

--- src/components/CSLogoIcon.js
+++ src/components/CSLogoIcon.js
@@ -4,7 +4,7 @@ export const CSLogoIcon = (props) => {
     return (
         <img {...props} style={{
             margin: '5px'
-        }} alt="logo" src={"/EGM_Logo_transparent.png"}/>
+        }} alt="logo" src={"/unibrain-logo.png"}/>
     )
 }
 export default CSLogoIcon
