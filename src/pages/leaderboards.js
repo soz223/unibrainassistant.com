99d6d2f93757ced6e2f4a87c898e34e1c381351a
soz223 <soz223@lehigh.edu>
2025-06-20 14:37:57 -0400

    update demo page with video

--- src/pages/leaderboards.js
+++ src/pages/leaderboards.js
@@ -23,9 +23,21 @@ const DemoPage = () => {
         }}
       />
 
-      <Typography variant="h5" color="textSecondary">
-        Building…
+      <Typography variant="h5" color="textSecondary" align="center">
+        Below is a demo video of our internal version, including how to upload files, what the system can do, and how to trigger processing.
       </Typography>
+
+      <div style={{ marginTop: '40px', width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <iframe
+          width="800"
+          height="450"
+          src="https://www.youtube.com/embed/uGr_NrG68GE"
+          title="Internal Demo Video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     </Container>
   )
 }
